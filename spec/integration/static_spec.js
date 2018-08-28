45bcd79c793ab77ef91589234a5f9e477035c536
David Probst <probst.dj2@gmail.com>
2018-08-27 20:09:18 -0500

    add about us test and route

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,7 +5,6 @@ const base = "http://localhost:3000/";
 describe("routes : static", () => {
 
   describe("GET /", () => {
-    console.log("here");
     it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
@@ -15,14 +14,14 @@ describe("routes : static", () => {
     });
   });
 
-  xdescribe("GET /" () => {
+  describe("GET /", () => {
     it("should return status code 200 and have 'About Us' in the body of the response", (done) => {
       request.get(base + "about", (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("About Us");
         done();
-      })
-    })
-  })
+      });
+    });
+  });
 
 });
