8614abda677ee2fd0542a7211f2e4b639643d2cd
David Probst <probst.dj2@gmail.com>
2018-09-05 21:03:54 -0500

    error on edit funcitonality

--- src/routes/advertisements.js
+++ src/routes/advertisements.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const advertisementController = require("..controllers/advertisementController");
+const advertisementController = require("../controllers/advertisementController");
 
 router.get("/advertisements", advertisementController.index);
+router.get("/advertisements/new", advertisementController.new);
+router.get("/advertisements/:id", advertisementController.show);
+// router.get("/advertisements/:id/edit", advertisementController.edit);
+router.post("/advertisements/create", advertisementController.create);
+router.post("/advertisements/:id/destroy", advertisementController.destroy);
 
 module.exports = router;
