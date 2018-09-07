f21c66f125840b0318985fafad406157f15197ab
David Probst <probst.dj2@gmail.com>
2018-09-06 20:58:16 -0500

    add edit functionality

--- src/routes/advertisements.js
+++ src/routes/advertisements.js
@@ -6,6 +6,7 @@ const advertisementController = require("../controllers/advertisementController"
 router.get("/advertisements", advertisementController.index);
 router.get("/advertisements/new", advertisementController.new);
 router.get("/advertisements/:id", advertisementController.show);
+ router.get("/advertisements/:id/edit", advertisementController.edit);
 router.post("/advertisements/create", advertisementController.create);
 router.post("/advertisements/:id/destroy", advertisementController.destroy);
 
