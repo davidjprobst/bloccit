61d7c827b90698b017236e9fcd40b9901d3a6001
David Probst <probst.dj2@gmail.com>
2018-09-08 17:54:19 -0500

    add model tests for create and getPosts

--- spec/unit/topic_spec.js
+++ spec/unit/topic_spec.js
@@ -1,19 +1,50 @@
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
+const Post = require("../../src/db/models").Post;
 
 describe("Topic", () => {
+
+  beforeEach((done) => {
+    this.topic;
+    this.post;
+    sequelize.sync({force: true}).then((res) => {
+
+      Topic.create({
+        title: "Why we love cats",
+        description: "Photos of cats doing things that make humans love them"
+      })
+      .then((topic) => {
+        this.topic = topic;
+        Post.create({
+          title: "My cat is so funny",
+          body: "He does this weird thing.",
+          topicId: this.topic.id
+        })
+        .then((post) => {
+          this.post = post;
+          done();
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+    });
+
+  });
+
 
   describe("#create()", () => {
 
       it("should create a topic object with a title and description", (done) => {
         Topic.create({
-          title: "Pros of Cryosleep during the long journey",
-          description: "1. Not having to answer the 'are we there yet?' question."
+          title: "Cats do a lot of weird things",
+          description: "Here's a cat stuck in a cardboard box"
         })
         .then((topic) => {
 
-          expect(topic.title).toBe("Pros of Cryosleep during the long journey");
-          expect(topic.description).toBe("1. Not having to answer the 'are we there yet?' question.");
+          expect(topic.title).toBe("Cats do a lot of weird things");
+          expect(topic.description).toBe("Here's a cat stuck in a cardboard box");
           done();
 
         })
@@ -23,6 +54,32 @@ describe("Topic", () => {
         });
       });
 
+      it("should not create a topic with missing title or description", (done) => {
+         Topic.create({
+           title: "Cats doing cat things"
+         })
+         .then((topic) => {
+           done();
+         })
+         .catch((err) => {
+           expect(err.message).toContain("Topic.description cannot be null");
+           done();
+         })
+       });
   });
+
+  describe("#getPost()", () => {
+
+      it("should return all associates posts", (done) => {
+
+        this.topic.getPosts()
+        .then((associatedPosts) => {
+          expect(associatedPosts.map(post => post.title)).toContain("My cat is so funny");
+          done();
+        });
+
+      });
+
+    });
 
 });
