16a91a0dda080c175bfb4c45f263759d12981c29
David Probst <probst.dj2@gmail.com>
2018-09-08 13:22:11 -0500

    Revert "Merge pull request #8 from davidjprobst/assignment-6" revert merging assignment-6 work
    This reverts commit 120c6d838e41b2cd48a4820c890fe981ef1f36d0, reversing
    changes made to 75fda05e965e4070f6517856e52c64913133fbfd.

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,11 +1,9 @@
 module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
-    const topicRoutes = require("../routes/topics");
-    const advertisementRoutes = require("../routes/advertisements");
+    const topicRoutes = require("../routes/topics")
 
     app.use(staticRoutes);
     app.use(topicRoutes);
-    app.use(advertisementRoutes);
   }
 }
