51fc2226ab4177c9d5585ea4f428ba5bbf6bb499
David Probst <probst.dj2@gmail.com>
2018-09-09 10:42:36 -0500

    add delete action and tests

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -92,4 +92,20 @@ describe("routes : posts", () => {
 
   });
 
+  describe("POST /topics/:topicId/posts/:id/destroy", () => {
+
+    it("should delete the post with the associate post ID", (done) => {
+      expect(this.post.id).toBe(1);
+      request.post(`${base}/${this.topic.id}/posts/${this.post.id}/destroy`, (err, res, body) => {
+        Post.findById(1)
+        .then((post) => {
+          expect(err).toBeNull();
+          expect(post).toBeNull();
+          done();
+        })
+      });
+    });
+
+  });
+
 });
