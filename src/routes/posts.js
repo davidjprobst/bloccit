37b3a5c272cf8b2cc5518ee68a608469fba9fa80
David Probst <probst.dj2@gmail.com>
2018-09-13 20:41:59 -0500

    fix failing update spec

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -9,6 +9,6 @@ router.get("/topics/:topicId/posts/:id", postController.show);
 router.get("/topics/:topicId/posts/:id/edit", postController.edit);
 router.post("/topics/:topicId/posts/create", validation.validatePosts, postController.create);
 router.post("/topics/:topicId/posts/:id/destroy", postController.destroy);
-router.post("/topics/:topicId/posts/:id/update", postController.update);
+router.post("/topics/:topicId/posts/:id/update", validation.validatePosts, postController.update);
 
 module.exports = router;
