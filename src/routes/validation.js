e287559631c1742d06c7240dcf5db60ee73480dd
David Probst <probst.dj2@gmail.com>
2018-09-13 20:34:03 -0500

    complete assignment work with passing specs

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -21,7 +21,6 @@ module.exports = {
   validateTopics(req, res, next) {
 
     if(req.method === "POST") {
-      req.checkParams("topicId", "must be valid").notEmpty().isInt();
       req.checkBody("title", "must be at least 5 characters in length").isLength({min: 5});
       req.checkBody("description", "must be at least 10 characters in length").isLength({min: 10});
     }
