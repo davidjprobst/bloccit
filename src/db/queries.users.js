2b58409450e099fcf0ede65307b1a9d32dc03f85
David Probst <probst.dj2@gmail.com>
2018-09-17 20:34:17 -0500

    users/sign_in spec failing

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -1,12 +1,12 @@
- constconst User =  User = requirerequire(("./models""./models").User;).User;
- constconst bcrypt =  bcrypt = requirerequire(("bcryptjs""bcryptjs"););
+// #1
+const User = require("./models").User;
+const bcrypt = require("bcryptjs");
 
- modulemodule.exports = {.exports = {
-   createUser(newUser, callback){  createUser(newUser, callback)
+module.exports = {
 
+  createUser(newUser, callback){
     const salt = bcrypt.genSaltSync();
     const hashedPassword = bcrypt.hashSync(newUser.password, salt);
-
     return User.create({
       email: newUser.email,
       password: hashedPassword
